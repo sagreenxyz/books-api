1dbeea987aef82167a63eda881323e53cca2f01c
Scott Green <sagreenxyz@gmail.com>
2022-03-16 15:44:17 -0500

    creating books-seed data file

---
+++ seeds/books-seed.js
@@ -0,0 +1,30 @@
+module.exports = [
+    {
+      "title": "The Shinobi Initiative",
+      "description": "The reality-bending adventures of a clandestine service agency in the year 2166",
+      "year": 2014,
+      "quantity": 10,
+      "imageURL": "www.sagreenxyz.com/assets/shinobi-initiative.jpeg"
+    },
+    {
+      "title": "Tess the Wonder Dog",
+      "description": "The tale of a dog who gets super powers",
+      "year": 2007,
+      "quantity": 3,
+      "imageURL": "www.sagreenxyz.com/assets/tess.jpg"
+    },
+    {
+      "title": "The Annals of Arathrae",
+      "description": "This anthology tells the intertwined narratives of six fairy tales.",
+      "year": 2016,
+      "quantity": 8,
+      "imageURL": "www.sagreenxyz.com/assets/arathrae.jpeg"
+    },
+    {
+      "title": "W∀RP",
+      "description": "A time-space anomaly folds matter from different points in earth's history in on itself, sending six unlikely heroes on a race against time as worlds literally collide.",
+      "year": 2010,
+      "quantity": 4,
+      "imageURL": "www.sagreenxyz.com/assets/warp.jpeg"
+    }
+  ]
