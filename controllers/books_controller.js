2ac7f6bffbfbf804eafff54c946617b13aa08882
Scott Green <sagreenxyz@gmail.com>
2022-03-16 15:33:43 -0500

    completing framework for implementing the books route in the books controller

--- controllers/books_controller.js
+++ controllers/books_controller.js
@@ -2,4 +2,9 @@ const express = require('express');
 const books = express.Router();
 const Book = require('../models/books.js');
 
+// INDEX http://localhost:[PORT]/books/
+books.get('/', (req, res) => {
+    res.status(200).send(`books-api books controller for route  http://localhost:[PORT]/books/`);
+});
+
 module.exports = books;
