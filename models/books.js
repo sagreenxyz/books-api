c3402a8f406c0f6f3f38589b1e72d470e15a3129
Scott Green <sagreenxyz@gmail.com>
2022-03-16 15:04:07 -0500

    setting book schema imageURL default to a placeholder image URL

--- models/books.js
+++ models/books.js
@@ -6,7 +6,7 @@ const bookSchema = new Schema({
     description: { type: String },
     year: { type: Number},
     quantity: { type: Number},
-    imageURL: { type: String }
+    imageURL: { type: String, default: 'https://via.placeholder.com/300x300' }
 });
 
 const Book = mongoose.model('Books', bookSchema); // connect bookSchema to 'Books' collection as a model and store in variable, Book
