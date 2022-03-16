4d44a7a3f490514afda90369ad51d4ab122dfc60
Scott Green <sagreenxyz@gmail.com>
2022-03-16 15:57:25 -0500

    setting up seed data route

--- controllers/books_controller.js
+++ controllers/books_controller.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const books = express.Router();
 const Book = require('../models/books.js');
+const seedData = require('../seeds/books-seed.js');
 
 // INDEX http://localhost:[PORT]/books/
 books.get('/', (req, res) => {
     res.status(200).send(`books-api books controller for route  http://localhost:[PORT]/books/`);
+});
+
+books.get('/data/seed', (req, res) => {
+        Book.insertMany(seedData)
 });
 
 module.exports = books;
