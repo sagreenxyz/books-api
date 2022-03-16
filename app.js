746ed2bee878fb4f60b6dc31409c8d6c1f56e7a8
Scott Green <sagreenxyz@gmail.com>
2022-03-16 14:48:44 -0500

    setup and test root route

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
+// DEPENDENCIES
 const express = require('express');
 
+//CONFIGURATION
 require('dotenv').config();
 const PORT = process.env.PORT || 5000;
-console.log(PORT);
+const app = express();
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('books-api GET /');
+});
+
+// LISTEN
+app.listen(PORT, () => {
+    console.log(`books-api server running at http://localhost:${PORT}`);
+});
