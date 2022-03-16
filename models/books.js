4a37ff0b8ee12f5deaf1a9037b0d206704630f26
Scott Green <sagreenxyz@gmail.com>
2022-03-16 15:02:37 -0500

    populating book schema data types

--- models/books.js
+++ models/books.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const bookSchema = new Schema({
-
+    title: { type: String },
+    description: { type: String },
+    year: { type: Number},
+    quantity: { type: Number},
+    imageURL: { type: String }
 });
 
 const Book = mongoose.model('Books', bookSchema); // connect bookSchema to 'Books' collection as a model and store in variable, Book
