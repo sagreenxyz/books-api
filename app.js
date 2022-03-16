5d4ab7f8cfb6fd3d45eacc0d2a08ec2b0ff10ab9
Scott Green <sagreenxyz@gmail.com>
2022-03-16 14:55:04 -0500

    configured mongoose connection to mongodb

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 // DEPENDENCIES
 const express = require('express');
+const mongoose = require('mongoose');
 
 //CONFIGURATION
 require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 const app = express();
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+        console.log('Connected to MongoDB at: ', process.env.MONGO_URI);
+    }
+);
 
 // MIDDLEWARE
 app.use(express.json());
